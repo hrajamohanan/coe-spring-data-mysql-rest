fbefd9f798fc3890bcd9181c11b79a3bfa5517d8
ranajit-jana <ranajit.jana@wipro.com>
2018-04-24 13:12:06 +0530

    checking in the  mysql -rest file

---
+++ src/main/java/com/wipro/coe/microservices/data/promotions/PromotionRepository.java
@@ -0,0 +1,21 @@
+package com.wipro.coe.microservices.data.promotions;
+
+
+//package com.wipro.coe.microservices.data.promotions;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.wipro.coe.microservices.data.promotions.entity.Promotion;
+
+
+@Transactional
+
+public interface PromotionRepository extends JpaRepository <Promotion, Long>{
+	
+
+
+
+}
